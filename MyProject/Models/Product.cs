18eb4939e54ae83ca3b18b663db46bbfa0330d29
Sumana269 <sumana.kammati@gmail.com>
2020-09-29 10:51:16 +0530

    Product Is Added

--- MyProject/Models/Product.cs
+++ MyProject/Models/Product.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyProject.Controllers
+namespace MyProject.Models
 {
     public class Product
     {
